48f53e7231da7b024baeb5b2cbad1b491167243a
Julien <julien.moura@gmail.com>
2013-05-13 17:12:30 -0500

    rss date checker: first step

---
+++ RssDateChecker.py
@@ -0,0 +1,38 @@
+﻿#-*-coding: utf-8-*-
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name:        RSS date checker
+# Purpose:     Check the last update date of a RSS and compare it to a date
+#               stored in a text file. If it's newer, then executes another
+#               program (typically a parser).
+#
+# Author:      Guts
+#
+# Created:     05/07/2012
+# Updated:
+#-------------------------------------------------------------------------------
+
+###################################
+######## Libraries import #########
+###################################
+# Standard library
+
+
+# 3rd party libraries
+
+
+# Custom modules
+
+
+###################################
+############# Classes #############
+###################################
+
+class RssDateChecker:
+    """  """
+    def __init__(self):
+        print 'initialized'
+
+    def extract_date(self):
+        print 'extracted'
+
