2b58c885ac2c8247c43040470a4c4b2d6304ee99
Guts <julien.moura@gmail.com>
2013-05-18 14:11:21 -0500

    added: date checker

--- RssDateChecker.py
+++ RssDateChecker.py
@@ -16,10 +16,12 @@
 ######## Libraries import #########
 ###################################
 # Standard library
-
+import datetime
+import pickle
+from os import path
 
 # 3rd party libraries
-
+from feedparser import parse
 
 # Custom modules
 
@@ -30,9 +32,29 @@
 
 class RssDateChecker:
     """  """
-    def __init__(self):
+    def __init__(self, flux, nom):
         print 'initialized'
+        self.last_EC = self.extract_last_date(flux, nom)
+        self.rss_EC = self.extract_rss_date(flux)
 
-    def extract_date(self):
-        print 'extracted'
+    def extract_last_date(self, flux, nom):
+        fic = "lastupdate_" + nom + ".txt"
+        with open(path.join("data", fic), "r") as f:
+            last_date = pickle.load(f)
+        # End of function
+        return last_date
+
+    def extract_rss_date(self, flux):
+        rss_date = parse(flux_EC).modified_parsed
+        return rss_date
+
+
+
+################################################################################
+if __name__ == '__main__':
+    flux_EC = "http://elcomercio.feedsportal.com/rss/portada.xml"
+    nom = "EC"
+    app = RssDateChecker(flux_EC, nom)
+
+
 
